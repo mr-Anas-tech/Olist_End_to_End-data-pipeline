967c49e35ae1939c1d01e4cca1bb89636f61878d
Muhammad Anas <mranaskhudabukh@gmail.com>
2026-05-25 11:14:09 +0000

    marts fact & dim

---
+++ models/olist/marts/dim_olist_customers.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized='table'
+) }}
+
+with customers as(
+    select * from
+    {{ ref('stg_olist_customers') }}
+),
+
+final as(
+    select
+    customer_id,
+    customer_unique_id,
+    customer_zip_code_prefix,
+    customer_city,
+    customer_state#
+    from customers
+)
+
+select * from final
